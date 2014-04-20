bbd2ea64d27170efbf0d1d0449ff9ba69d098c5e
Jonathan Chauncey <jchauncey@rallydev.com>
2014-04-19 21:57:08 -0600

    Revert "going to use Berkshelf::API::VERSION instead of trying to use fileio to read the VERSION file"
    This reverts commit 91f27df06097e30f0dd000f5b7d13150a27b46d3.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jchauncey@rallydev.com'
 license          'MIT'
 description      'Prints profling information for a chef run'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          Berkshelf::API::VERSION
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
 depends 'chef_handler'
