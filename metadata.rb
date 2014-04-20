268e5e9a038989cbcea8cc3693625b8d1380ac18
Jonathan Chauncey <jchauncey@rallydev.com>
2014-04-19 21:58:16 -0600

    adding rescue to the file read

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jchauncey@rallydev.com'
 license          'MIT'
 description      'Prints profling information for a chef run'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
 depends 'chef_handler'
